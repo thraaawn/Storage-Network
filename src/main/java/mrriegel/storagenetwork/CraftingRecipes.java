46d024ffddaec714cc787099e2b38c0176299498
sam <samson.bassett@gmail.com>
2017-06-15 22:04:42 -0700

    json recipes created.. mostly work , need to figure out load order of remotes

--- src/main/java/mrriegel/storagenetwork/CraftingRecipes.java
+++ src/main/java/mrriegel/storagenetwork/CraftingRecipes.java
@@ -24,12 +24,32 @@ public class CraftingRecipes {
     
     
 //    GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.master), "dkd", "kck", "dkd", 'd', Blocks.QUARTZ_BLOCK, 'k', ModBlocks.kabel, 'c', Items.DIAMOND);
+    
+    
+    
 //    GameRegistry.addShapedRecipe(new ItemStack(ModItems.upgrade, 1, 0), " c ", "gig", " c ", 'c', Blocks.REDSTONE_BLOCK, 'i', ModBlocks.kabel, 'g', Items.GOLD_INGOT);
+    
+    
 //    GameRegistry.addShapedRecipe(new ItemStack(ModItems.upgrade, 1, 1), " c ", "rir", " c ", 'c', Items.COMPARATOR, 'i', ModBlocks.kabel, 'r', Items.REDSTONE);
-//    GameRegistry.addShapedRecipe(new ItemStack(ModItems.upgrade, 1, 2), " c ", "gig", " c ", 'c', Blocks.REDSTONE_BLOCK, 'i', ModBlocks.kabel, 'g', Items.BLAZE_POWDER);
+    
+    
+    
+//     GameRegistry.addShapedRecipe(new ItemStack(ModItems.upgrade, 1, 2), " c ", "gig", " c ", 'c', Blocks.REDSTONE_BLOCK, 'i', ModBlocks.kabel, 'g', Items.BLAZE_POWDER);
+    
+    
+    
+    
 //    GameRegistry.addShapedRecipe(new ItemStack(ModItems.upgrade, 1, 3), "c", "i", "c", 'c', Items.COMPARATOR, 'i', ModBlocks.kabel);
+    
+    
+    
 //    GameRegistry.addShapedRecipe(new ItemStack(ModItems.remote, 1, 0), " c ", "eie", " c ", 'c', Items.GOLD_INGOT, 'i', ModBlocks.kabel, 'e', Items.ENDER_PEARL);
-//    GameRegistry.addShapedRecipe(new ItemStack(ModItems.remote, 1, 1), "pcp", "mim", "pdp", 'c', Blocks.EMERALD_ORE, 'i', ModItems.remote, 'd', Blocks.DIAMOND_ORE, 'm', ModBlocks.master, 'p', Items.ENDER_PEARL);
+    
+    
+    
+    
+//    GameRegistry.addShapedRecipe(new ItemStack(ModItems.remote, 1, 1), "pcp", "mim", "pdp", 'c', Blocks.EMERALD_ORE, 'i', ModItems.remote, 'd', Blocks.DIAMOND_ORE, 
+    //'m', ModBlocks.master, 'p', Items.ENDER_PEARL);
 //  
 //    
     
